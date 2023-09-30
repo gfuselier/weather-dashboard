bba97caba0c826e9269a999956dd63d6202f5391
gfuselier <gisellefuselier@gmail.com>
2023-09-30 11:40:51 -0500

    added api key and fixed typo in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,17 @@ var searchBox = document.getElementById("search-box")
 searchBtn.addEventListener("click", function(event) {
     event.preventDefault();
     console.log(event)
-    var city = searchBox.value
+    var city = searchBox.value.trim()
     console.log(city);
 
     var history = JSON.parse(localStorage.getItem("cities")) || []
     history.push(city)
     localStorage.setItem("cities",JSON.stringify(history))
 
-    //make the url! with city and api key
+    var APIKey = "ddd62cc0a10cf570b502b56b15243447"
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    
+    fetch(queryURL)
     //fetch!
     //target and update html with data
 }
