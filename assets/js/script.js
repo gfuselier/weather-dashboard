4bd6ae10d79b45bc103c759cfbc3b31c7c633f1d
gfuselier <gisellefuselier@gmail.com>
2023-10-01 19:30:27 -0500

    added temp, wind, and humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var searchBtn = document.getElementById("search-btn")
 var searchBox = document.getElementById("search-box")
+var cityName = document.getElementById("city-name")
+var temp = document.getElementById("temp")
+var wind = document.getElementById("wind")
+var humidity = document.getElementById("humidity")
 
 searchBtn.addEventListener("click", function(event) {
     event.preventDefault();
@@ -12,7 +16,7 @@ searchBtn.addEventListener("click", function(event) {
     localStorage.setItem("cities",JSON.stringify(history))
 
     var APIKey = "ddd62cc0a10cf570b502b56b15243447"
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
     
     fetch(queryURL)
         .then(function(response) {
@@ -20,8 +24,13 @@ searchBtn.addEventListener("click", function(event) {
         })
         .then(function(data) {
             console.log(data)
+            console.log(data.main.temp)
+            console.log(data.wind.speed)
+            temp.textContent = "Temp: " + data.main.temp + "°F"
+            wind.textContent = "Wind: " + data.wind.speed + " MPH"
+            humidity.textContent = "Humidity: " + data.main.humidity + "%"
         })
-    //it works!
+    
     //target and update html with data
 }
 )
