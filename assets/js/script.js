7430665e0678737eeb8ce65bd1b795314bd61e9d
gfuselier <gisellefuselier@gmail.com>
2023-10-01 21:40:28 -0500

    added 5 day url fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,9 +45,21 @@ searchBtn.addEventListener("click", function(event) {
             temp.textContent = "Temp: " + data.main.temp + "°F"
             wind.textContent = "Wind: " + data.wind.speed + " MPH"
             humidity.textContent = "Humidity: " + data.main.humidity + "%"
+
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+            var fiveDayUrl =  "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
+            //second fetch gets five-day forecast
+            fetch(fiveDayUrl)
+                .then(function(result) {
+                    return result.json();
+                })
+                .then (function(data) {
+                    console.log(data)
+                })
         });
 
-    var fiveDayUrl =  "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
+    
 })
 
 //click on history and get the data again
